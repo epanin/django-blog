6b636d3ffca91c1776439fc5bba8155e93d77833
eugene <e.u.panin@yandex.ru>
2024-02-19 15:18:47 +0300

    dynamic routes

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-from typing import Any, Dict
+from typing import Any
 
 from django.views import View
+
+from django.shortcuts import redirect
+
+from django.urls import reverse
 
 
 class ArticleView(View):
     
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.context = {'appname': 'Articles!'}
+        self.context = {
+            'appname': 'Articles!',
+        }
         self.template_name = 'articles/index.html'
     
     
+    
     def get(self, *args, **kwargs):
-        return render(self.request, self.template_name, self.context)
+        
+        tag = kwargs.get('tag', None)
+        article_id = kwargs.get('article_id', None)
+        if tag and article_id:
+            self.context['tag'] = tag
+            self.context['article_id'] = article_id
+            return render(self.request, self.template_name, self.context)
+
+        return redirect(reverse('article', kwargs={'tag': 'python', 'article_id': 42}))            
+         
