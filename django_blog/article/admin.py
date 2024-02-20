c7b7aae0320d599731210ea1001bfca7ebe353b4
eugene <e.u.panin@yandex.ru>
2024-02-20 12:00:39 +0300

    add Article model to admin, env vars moved to .env

--- django_blog/article/admin.py
+++ django_blog/article/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.admin import DateFieldListFilter 
+
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ('name', 'body')
+    list_filter = (('timestamp', DateFieldListFilter), )
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv  # Импортируем environ    
+
+import os
+
+load_dotenv()  # Загрузка переменных окружения из файла .env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!h_y8@-s(#)gp=syo$-dhft5lyw2riq%n$e8y1jib=1q0wg!x'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
